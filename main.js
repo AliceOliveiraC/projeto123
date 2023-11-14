ded12100014a9ffaa5988e178f2b6aa760e0333e
AliceOliveiraC <126628935+AliceOliveiraC@users.noreply.github.com>
2023-11-14 19:50:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function Preload()
+{   
+
+}
+
+function setup()
+{
+    canvas = createCanvas(500,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+}
+
+function draw()
+{
+    
+}
